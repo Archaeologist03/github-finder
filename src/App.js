42caa9e55e363650769ccb18812d5996b8e7b324
Archaeologist03 <omniiknight@hotmail.com>
2019-11-30 23:02:12 +0100

    Alert context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
@@ -9,42 +9,37 @@ import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 import './App.css';
 
 const App = () => {
-	const [alert, setAlert] = useState(null);
-
-	const showAlert = (msg, type) => {
-		setAlert({ msg, type });
-
-		setTimeout(() => setAlert(null), 5000);
-	};
-
 	return (
 		<GithubState>
-			<Router>
-				<div className='App'>
-					<Navbar title='Github Finder' icon='fab fa-github' />
-					<div className='container'>
-						<Alert alert={alert} />
-						<Switch>
-							<Route
-								exact
-								path='/'
-								render={props => (
-									<Fragment>
-										<Search setAlert={showAlert} />
-										<Users />
-									</Fragment>
-								)}
-							/>
-							<Route exact path='/about' component={About} />
-						</Switch>
-						<Route exact path='/user/:login' component={User} />
+			<AlertState>
+				<Router>
+					<div className='App'>
+						<Navbar title='Github Finder' icon='fab fa-github' />
+						<div className='container'>
+							<Alert />
+							<Switch>
+								<Route
+									exact
+									path='/'
+									render={props => (
+										<Fragment>
+											<Search />
+											<Users />
+										</Fragment>
+									)}
+								/>
+								<Route exact path='/about' component={About} />
+							</Switch>
+							<Route exact path='/user/:login' component={User} />
+						</div>
 					</div>
-				</div>
-			</Router>
+				</Router>
+			</AlertState>
 		</GithubState>
 	);
 };
